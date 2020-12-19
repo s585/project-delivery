0ef1a873d143df98e5a4e33094e267afaecbcb17
s585 <andrey.svetov@gmail.com>
2020-12-19 19:56:07 +0300

    initial commit

---
+++ src/main/java/tech/itpark/projectdelivery/ProjectDeliveryApplication.java
@@ -0,0 +1,13 @@
+package tech.itpark.projectdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectDeliveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/itpark/projectdelivery/ProjectDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.itpark.projectdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDeliveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
